1075fcc8dd6de4c986b2783668e6e76f78c9c855
Lucian Qin <2440557756@qq.com>
2021-09-28 22:27:13 +1000

    Added Testimonial component

---
+++ src/components/Components/Components/Title/Title.style.tsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+export const TitleName = styled.h1`
+
+`
+
+export const ColorBar = styled.div`
+background-color: #BFEEE9;
+max-width: 350px;
+height: 5px;
+`

b07ab29e061669016ccdb722549c0e4853cf7ded
yiyang-rose <zxmyqq@126.com>
2021-09-28 12:05:20 +0800

    success

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,12 @@
-import Hero from "components/Hero/Hero"
 import React from "react"
-import { ThemeProvider } from "styled-components"
-import { createGlobalStyle } from "styled-components"
-import theme from "styles/theme"
 
-const GlobalStyles = createGlobalStyle`
-  @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;700&display=swap');
-  body {
-    font-family: 'Roboto', sans-serif;
-  }
-`
+import NavBar from "./components/Navbar/Navbar"
 
 function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <GlobalStyles />
-      <Hero />
-    </ThemeProvider>
+    <div className="App">
+      <NavBar />
+    </div>
   )
 }
 
--- src/components/Navbar/Down-part/Down-part.tsx
+++ src/components/Navbar/Down-part/Down-part.tsx
@@ -1,73 +1,76 @@
-import { stringify } from "querystring"
+import { KeyboardArrowDown as KeyboardArrowDownIcon } from "@material-ui/icons"
 import React from "react"
 import styled from "styled-components"
-import { KeyboardArrowDown as KeyboardArrowDownIcon } from "@material-ui/icons"
 
 const DIV = styled.div`
-width: 100%;
-height: 80%;
-background-color: #3BCBBE;
-position: relative;
-display: flex;
-justify-content: space-between;
+  width: 100%;
+  height: 80%;
+  background-color: #3bcbbe;
+  position: relative;
+  display: flex;
+  justify-content: space-between;
 `
 const STRONG = styled.strong`
-position: relative;
-top: 20%;
-left: 5%;
-font-size: 25px;
-color: white;
-align-item: center;
+  position: relative;
+  top: 20%;
+  left: 5%;
+  font-size: 25px;
+  color: white;
+  align-item: center;
 `
-const UL = styled.ul `
-width: 600px;
-top: 20%;
-right: 10%;
-display: flex;
-position: relative;
-font-size: 15px;
-color: white;
-flex-direction: row;
-list-style: none;
-justify-content: space-between;
+const UL = styled.ul`
+  width: 600px;
+  top: 20%;
+  right: 10%;
+  display: flex;
+  position: relative;
+  font-size: 15px;
+  color: white;
+  flex-direction: row;
+  list-style: none;
+  justify-content: space-between;
 `
 
 const LI = styled.li`
-list-style-type: none;
-align-item: center;
-.title{
+  list-style-type: none;
+  align-item: center;
+  .title {
     bottom: 15%;
-}
+  }
 `
 
 const ALink = styled.a<{ white?: boolean }>`
 position: relative;
 text-decoration: none;
 font-weight:bold;
-${({white }) => white ? 'background-color: white; color: black' : 'background-color:#3BCBBE; color: white'};
+${({ white }) => (white ? "background-color: white; color: black" : "background-color:#3BCBBE; color: white")};
 border-radius: 10px;
 padding 15px;
 `
 
 const Down = () => (
-    <DIV>
-        <STRONG>Cuckoo</STRONG>
-        <UL>
-            <LI>
-                <ALink href="#">Home</ALink>
-            </LI>
-            <LI>
-                <ALink href="#" className="title">PTE Practice</ALink>
-                <KeyboardArrowDownIcon></KeyboardArrowDownIcon>
-            </LI>
-            <LI>
-                <ALink href="#">Login</ALink>
-            </LI>
-            <LI>
-                <ALink href="#" white>Sign up</ALink>
-            </LI>
-        </UL>
-    </DIV>
+  <DIV>
+    <STRONG>Cuckoo</STRONG>
+    <UL>
+      <LI>
+        <ALink href="#">Home</ALink>
+      </LI>
+      <LI>
+        <ALink href="#" className="title">
+          PTE Practice
+        </ALink>
+        <KeyboardArrowDownIcon></KeyboardArrowDownIcon>
+      </LI>
+      <LI>
+        <ALink href="#">Login</ALink>
+      </LI>
+      <LI>
+        <ALink href="#" white>
+          Sign up
+        </ALink>
+      </LI>
+    </UL>
+  </DIV>
 )
-    
+
 export default Down
